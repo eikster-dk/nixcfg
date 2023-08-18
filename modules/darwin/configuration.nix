acb3db7d308ca6657807e22256be2f752b7b9812
Eik Emil Bengtsson <me@eikc.dk>
2023-08-18 14:56:32 +0200

    chore: added system.default.dock settings

--- modules/darwin/configuration.nix
+++ modules/darwin/configuration.nix
@@ -13,7 +13,18 @@
   system = {
     defaults = {
       dock = {
-        autohide = false;
+        autohide = true;
+        autohide-delay = 0.0;
+        orientation = "left";
+        tilesize = 35;
+        showhidden = true;
+        show-recents = true;
+        show-process-indicators = true;
+        expose-animation-duration = 0.1;
+        expose-group-by-app = true;
+        launchanim = false;
+        mineffect = "scale";
+        mru-spaces = false;
       };
     };
   };
