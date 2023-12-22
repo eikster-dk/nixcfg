0882e33c2f24882345481ae988384006a5f58eff
Eik Emil Bengtsson <git.noreply@e16n.dev>
2023-12-22 13:42:42 +0100

    chore(pkgs): setup wanted packages from nixStore

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -1,9 +1,15 @@
 { pkgs, ... }: {
   home.packages = with pkgs; [
-   (pkgs.callPackage ../packages/scripts/default.nix { })
+    (pkgs.callPackage ../packages/scripts/default.nix { })
+    comma
+    fd
     go-task
-    zk
+    hurl
+    jq
     karabiner-elements
+    nodejs_20 #consider moving to nodenv?
+    ripgrep
+    zk
   ];
 }
 
