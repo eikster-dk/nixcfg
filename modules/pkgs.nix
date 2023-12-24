8ccfa0fa152495e90cf10ff9cadb0cdc4199b6aa
Eik Emil Bengtsson <git.noreply@e16n.dev>
2023-12-24 08:27:25 +0100

    chore(pkgs): ngrok and tree

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -1,4 +1,10 @@
 { pkgs, ... }: {
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      allowUnfreePredicate = (_: true);
+    };
+  };
   home.packages = with pkgs; [
     (pkgs.callPackage ../packages/scripts/default.nix { })
     comma
@@ -7,8 +13,10 @@
     hurl
     jq
     karabiner-elements
+    ngrok
     nodejs_20 #consider moving to nodenv?
     ripgrep
+    tree
     zk
   ];
 }
