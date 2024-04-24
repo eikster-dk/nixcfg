9bf75e7cbbc4d6ca76f8bc08face044546f03db2
Eik Emil Bengtsson <git.noreply@e16n.dev>
2024-04-24 15:23:25 +0200

    feat: added prettier for ts & js files

--- modules/neovim/config/lua/plugins/formatters.lua
+++ modules/neovim/config/lua/plugins/formatters.lua
@@ -1,8 +1,16 @@
 return {
   {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      table.insert(opts.ensure_installed, "prettier")
+    end,
+  },
+  {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        ["javascript"] = { "prettier" },
+        ["typescript"] = { "prettier" },
         templ = { "templ" },
       },
     },
