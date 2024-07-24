12ae7842fd8c2d6eff4f2a45ef1f84681f23265a
Eik Emil Bengtsson <git.noreply@e16n.dev>
2024-07-24 21:34:42 +0200

    feat(nvim): introduced flash

--- modules/neovim/v2/after/plugin/flash.lua
+++ modules/neovim/v2/after/plugin/flash.lua
@@ -1,14 +1,8 @@
 local flash = require("flash")
 flash.setup({})
 
-vim.keymap.set("n", "<leader>ls", function()
-  flash.jump()
-end, { desc = "Flash jump" })
-
-vim.keymap.set("n", "<leader>lt", function()
-  flash.treesitter()
-end, { desc = "Flash treesitter" })
-
-vim.keymap.set("n", "<leader>lr", function()
-  flash.treesitter_search()
-end, { desc = "Flash treesitter search" })
+vim.keymap.set({ "n", "x", "o" }, "<leader>ls", flash.jump, { desc = "Flash jump" })
+vim.keymap.set({ "n", "x", "o" }, "<leader>lt", flash.treesitter, { desc = "Flash treesitter" })
+vim.keymap.set("n", "<leader>lr", flash.treesitter_search, { desc = "Flash treesitter search" })
+vim.keymap.set({ "o", "x" }, "R", flash.treesitter_search, { desc = "Treesitter Search" })
+vim.keymap.set("c", "<c-s>", flash.toggle, { desc = "Toggle Flash Search" })
