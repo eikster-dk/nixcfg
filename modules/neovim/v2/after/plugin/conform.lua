f52071d2f8bf3db093c09c156d7f1ae4ae7927a1
Eik Emil Bengtsson <git.noreply@e16n.dev>
2024-08-28 11:17:36 +0200

    feat(nvim): enabled nixpkgs_fmt formatter in conform

--- modules/neovim/v2/after/plugin/conform.lua
+++ modules/neovim/v2/after/plugin/conform.lua
@@ -1,9 +1,10 @@
 require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
+    nix = { "nixpkgs_fmt" },
+    templ = { "templ" },
     ["javascript"] = { "prettier" },
     ["typescript"] = { "prettier" },
-    templ = { "templ" },
   },
 })
 
