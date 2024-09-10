9facdf53dce579a64172bcf13f123686079a3ea5
Eik Emil Bengtsson <git.noreply@e16n.dev>
2024-09-10 10:09:15 +0200

    feat(nvim): added pg_format to conform

--- modules/neovim/v2/after/plugin/conform.lua
+++ modules/neovim/v2/after/plugin/conform.lua
@@ -2,6 +2,7 @@ require("conform").setup({
   formatters_by_ft = {
     lua = { "stylua" },
     nix = { "nixpkgs_fmt" },
+    sql = { "pg_format" },
     templ = { "templ" },
     ["javascript"] = { "prettier" },
     ["typescript"] = { "prettier" },
