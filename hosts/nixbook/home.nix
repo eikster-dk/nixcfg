faa4a1cf954ed5c08ac2bcb122495a56d36cdca3
Eik Emil Bengtsson <git.noreply@e16n.dev>
2024-10-09 13:33:58 +0200

    chore: allowed 1password as unfree software

--- hosts/nixbook/home.nix
+++ hosts/nixbook/home.nix
@@ -6,6 +6,14 @@ let
   inherit (import ./variables.nix);
 in
 {
+  nixpkgs = {
+    config = {
+      allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+        "1password"
+      ];
+    };
+  };
+
   # Home Manager Settings
   home.username = "eikster";
   home.homeDirectory = "/home/eikster";
