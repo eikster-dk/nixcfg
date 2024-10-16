603e638a2354d3031fb158b36722aa3fad15e6cf
Eik Emil Bengtsson <git.noreply@e16n.dev>
2024-10-16 13:32:59 +0200

    chore(ssh): ssh settings cross system compatible

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -1,7 +1,14 @@
-{ config, ... }: {
+{ pkgs, ... }:
+let
+  onePassPath = (if pkgs.stdenv.isDarwin then "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" else "~/.1password/agent.sock");
+in
+{
   programs.ssh = {
     enable = true;
-    includes = [ "~/.ssh/config.local" ];
     serverAliveInterval = 60;
+    extraConfig = ''
+    Host *
+          IdentityAgent ${onePassPath}
+    '';
   };
 }
