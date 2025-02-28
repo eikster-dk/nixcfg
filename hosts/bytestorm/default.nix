19578589789643e3924db1aaf5dec7dee5622f8b
Eik Emil Bengtsson <git.noreply@e16n.dev>
2025-02-28 13:13:53 +0100

    chore: making my dotfiles compatible with darwin and unix

---
+++ hosts/bytestorm/default.nix
@@ -0,0 +1,44 @@
+{ config
+, lib
+, pkgs
+, inputs
+, modulesPath
+, ...
+}:
+{
+  imports = [
+    inputs.nixos-hardware.nixosModules.framework-13-7040-amd
+    inputs.self.nixosModules.eikster
+    (inputs.self + /users/eikster)
+  ];
+
+  config = {
+    system.stateVersion = "24.11";
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+
+    environment.defaultPackages = lib.mkForce [ ];
+
+    # Bootloader.
+    boot = {
+      loader.systemd-boot.enable = true;
+      loader.efi.canTouchEfiVariables = true;
+    };
+
+    # Enable networking
+    networking = {
+      hostName = "bytestorm";
+      hostId = "";
+
+      networkmanager.enable = true;
+    };
+
+    services.fwupd.enable = true;
+
+    features = {
+      _1password.enable = true;
+      nixSettings.enable = true;
+      agenix.enable = true;
+      primaryUsers.users = [ "eikster" ];
+    };
+  };
+}
--- users/eikster/home/default.nix
+++ users/eikster/home/default.nix
@@ -4,6 +4,34 @@
 , pkgs
 , ...
 }:
+let
+  isLinux = pkgs.system != "aarch64-darwin";
+  isDarwin = pkgs.system == "aarch64-darwin";
+
+  # Features common to all platforms
+  commonFeatures = {
+    cli = {
+      granted.enable = true;
+    };
+    suites = {
+      all.enable = true;
+    };
+  };
+
+  # Linux-specific features
+  linuxFeatures = lib.optionalAttrs isLinux {
+    desktop = {
+      hyprland = true;
+    };
+  };
+
+  # macOS-specific features
+  darwinFeatures = lib.optionalAttrs isDarwin {
+    macos = {
+      aerospace.enable = true;
+    };
+  };
+in
 {
   imports = [
     inputs.self.homeMangerModules.eikster
@@ -12,7 +40,7 @@
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.11";
   home.username = "eikster";
-  home.homeDirectory = "/Users/eikster";
+  home.homeDirectory = if isDarwin then "/Users/eikster" else "/home/eikster";
   home.sessionVariables = {
     EDITOR = "nvim";
     PROJECTS = "$HOME/code";
@@ -26,32 +54,22 @@
       dogdns
       fd
       go-task
-      #ngrok
       nodejs_20
       ripgrep
       rsync
       sqlc
       tailwindcss
-      tree
       ulid
-      zk
     ]
     ### darwin specific packages
-    ++ (lib.optionals (pkgs.system == "aarch64-darwin") [
+    ++ (lib.optionals (isDarwin) [
       ## todo, improve this
       (pkgs.callPackage ../../../packages/scripts/default.nix { })
       karabiner-elements
     ])
     ### nixos specific packages
-    ++ (lib.optionals (pkgs.system != "aarch64-darwin") [
+    ++ (lib.optionals (isLinux) [
     ]);
 
-  features = {
-    suites = {
-      all.enable = true;
-    };
-    macos = {
-      aerospace.enable = true;
-    };
-  };
+  features = commonFeatures // linuxFeatures // darwinFeatures;
 }
