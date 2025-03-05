9fd53c8ea7fcce5e26d3ad1dc8beffd75a49cf9e
Eik Emil Bengtsson <git.noreply@e16n.dev>
2025-03-05 21:49:14 +0100

    feat(hyprland): specialised hyprland settings

---
+++ users/eikster/home/hyprland/default.nix
@@ -0,0 +1,24 @@
+{ config
+, lib
+, pkgs
+, ...
+}: {
+  wayland.windowManager = {
+    hyprland.settings = {
+      binds = [
+        "$mainMod,Z&J,exec,${lib.getExe pkgs.vesktop}"
+      ];
+
+      windowrulev2 = [
+        "workspace special:magic,class:(vesktop)"
+      ];
+
+      workspace = [
+        "1,defaultName:terminal,on-created-empty:ghostty"
+        "2,defaultName:web,on-created-empty:firefox"
+        "3,defaultName:notes,on-created-empty:${lib.getExe' pkgs.obsidian "obsidian"}"
+        "4,defaultName:1Password,on-created-empty:1password"
+      ];
+    };
+  };
+}
