39369c820d77f16f366e65a0157096ddd5bcf0ba
Eik Emil Bengtsson <git.noreply@e16n.dev>
2025-03-06 14:32:07 +0100

    chore(secrets): missing key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,8 @@
 let
+  bytestorm = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILbXmipqKXFwKotxGrMW2XF+mdjeorJC6o1IR+EGyPsl";
   eikster = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINJdBV35he63Px4V/wGdEkvtj5OFlAcX4f/ovc9dePHa";
   users = [
+    bytestorm
     eikster
   ];
 
@@ -12,7 +14,7 @@ let
   all = users ++ servers;
 in
 {
-  "eikster.age".publicKeys = [ eikster ];
+  "eikster.age".publicKeys = [ eikster bytestorm ];
   "mibzy_tailscale.age".publicKeys = [ eikster mibzy ];
   "mibzy_meilisearch_key.age".publicKeys = [ eikster mibzy ];
 }
