8b8dcdc318a5d4ee26ec75d5c037517f3310f4a2
Eik Emil Bengtsson <git.noreply@e16n.dev>
2025-03-07 21:20:48 +0100

    feat(tpm2): enable auto decrypt of LUKS via TPM2

--- modules/hosts/lanzaboote/nixos.nix
+++ modules/hosts/lanzaboote/nixos.nix
@@ -6,9 +6,15 @@
 }:
 let
   cfg = config.features.boot.secureBoot;
+  cryptenroll = pkgs.writeShellScriptBin "cryptenroll" ''
+    #!/bin/sh
+    systemd-cryptenroll --tpm2-device=auto \
+      --tpm2-pcrs=0+2+7+12+15:sha256=0000000000000000000000000000000000000000000000000000000000000000 \
+      --wipe-slot=tpm2 $1
+  '';
+
 in
 {
-
   options.features.boot.secureBoot = {
     enable = lib.mkEnableOption "Enable secure boot with lanzaboote";
   };
@@ -17,11 +23,12 @@ in
   config = lib.mkIf cfg.enable {
 
     environment.systemPackages = [
+      cryptenroll
       pkgs.sbctl
     ];
 
     boot = {
-      # initrd.systemd.enable = true;
+      initrd.systemd.enable = true;
       loader.systemd-boot.enable = lib.mkForce false;
       lanzaboote = {
         enable = true;
